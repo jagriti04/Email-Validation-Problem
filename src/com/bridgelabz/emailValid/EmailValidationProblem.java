01f5e2be1dcfe3c1b086d3269aef1734f62201f0
Jagriti <jagriti.agni@gmail.com>
2020-09-28 23:35:39 +0530

    [Jagriti]ADDED: match strings starting with abc

--- src/com/bridgelabz/emailValid/EmailValidationProblem.java
+++ src/com/bridgelabz/emailValid/EmailValidationProblem.java
@@ -1,11 +1,32 @@
 package com.bridgelabz.emailValid;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidationProblem {
+	
+	public static final String[] emailArray =  {"abc", "abc@yahoo.com", "abc-100@yahoo.com", "cdf@g.com",
+											"abc@.com.my"}; 
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to the email validation system");
 		
+		for(String matchString : emailArray) {
+			String patternString = "^(abc)";
+			System.out.println("match email: " + matchString);
+			
+			Pattern pattern = Pattern.compile(patternString);
+		    Matcher matcher = pattern.matcher(matchString);
+		    boolean isMatch = matcher.find();
+		    
+			System.out.println(isMatch);
+			if (isMatch) {
+				System.out.println("The email given is valid");
+			} else {
+				System.out.println("The email given is invalid");
+			}			
+		}
 		
 	}
 }
