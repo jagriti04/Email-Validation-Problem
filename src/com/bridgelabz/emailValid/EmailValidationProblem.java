df0724357ba09a3f2554f0e77c42fc7140ea9cde
Jagriti <jagriti.agni@gmail.com>
2020-09-28 21:33:31 +0530

    [Jagriti]ADDED: welcome msg

---
+++ src/com/bridgelabz/emailValid/EmailValidationProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.emailValid;
+
+public class EmailValidationProblem {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the email validation system");
+		
+		
+	}
+}
